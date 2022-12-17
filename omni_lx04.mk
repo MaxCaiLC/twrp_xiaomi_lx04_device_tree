faff1c7726123aa8e6d6a577755a3aa88e5ac94d
MaxCaiLC <104661716+MaxCaiLC@users.noreply.github.com>
2022-12-17 20:00:42 +0800

    Update and rename omni_mi_lx04.mk to omni_lx04.mk

--- omni_lx04.mk
+++ omni_lx04.mk
@@ -21,16 +21,16 @@ $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Inherit from mi_lx04 device
-$(call inherit-product, device/xiaomi/mi_lx04/device.mk)
+# Inherit from lx04 device
+$(call inherit-product, device/xiaomi/lx04/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := mi_lx04
-PRODUCT_NAME := omni_mi_lx04
+PRODUCT_DEVICE := lx04
+PRODUCT_NAME := omni_lx04
 PRODUCT_BRAND := XiaoMi
 PRODUCT_MODEL := LX04
 PRODUCT_MANUFACTURER := xiaomi
