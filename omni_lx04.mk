ebc08944ffac76bf8908ce2b5361a103e72f785e
MaxCaiLC <104661716+MaxCaiLC@users.noreply.github.com>
2023-07-29 07:40:33 +0800

    hot fix

--- omni_lx04.mk
+++ omni_lx04.mk
@@ -29,6 +29,7 @@ $(call inherit-product, device/xiaomi/lx04/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lx04
