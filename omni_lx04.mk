b30258dc8d51d1f2bfd4a5814e35b9d24a91a55d
MaxCaiLC <104661716+MaxCaiLC@users.noreply.github.com>
2023-07-28 23:22:16 +0800

    Optimized compilation configuration

--- omni_lx04.mk
+++ omni_lx04.mk
@@ -16,17 +16,14 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+# Release name (automatically taken from this file's suffix)
+PRODUCT_RELEASE_NAME := $(lastword $(subst /, ,$(lastword $(subst _, ,$(firstword $(subst ., ,$(MAKEFILE_LIST)))))))
 
 # Inherit from mi_lx04 device
 $(call inherit-product, device/xiaomi/lx04/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lx04
