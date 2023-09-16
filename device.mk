90d2d2949722f3349529d7d4f269ce37cf9de513
MaxCaiLC <104661716+MaxCaiLC@users.noreply.github.com>
2023-09-16 09:57:06 +0800

    fix a bug

--- device.mk
+++ device.mk
@@ -16,6 +16,9 @@
 # limitations under the License.
 #
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
 LOCAL_PATH := device/xiaomi/lx04
 
 # A/B
